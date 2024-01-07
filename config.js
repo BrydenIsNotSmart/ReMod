01712e3aed02f20668911004ef432fa7ef6d7f48
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-01-07 15:47:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     port: 3006,
-      mongoURI: "mongodb+srv://vitaldevelopment:n3yN715890SwFY1X@serverlessinstance0.afbkiui.mongodb.net/ReMod?retryWrites=true&w=majority",
+      mongoURI: "dburl",
          ownerids: ["01GPZ5PTPQ2RNMZEF02NKD7TQE"],
            bot: {
              prefix: "!",
-             token: "2F3uFWdFw_vp1hXhN9gl9_ZkNFKBypCiCFgefEwZVnSvnYyFQWeW5cOxSwNPqci7"
+             token: "token"
            },
         }
